611eb46b073a65b0044957f3d8b79d04fd7d4e9a
Kennya <castro21.kc@gmail.com>
2020-05-11 15:51:15 -0600

    Agregue primero.php

---
+++ primero.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculo Promedios</title>
+</head>
+
+<body bgcolor="#F5D0A9"><center>
+    <h1>Elabore un programa que lea una lista de nombres de aves en cualquier orden y luego las ordene en orden alfabético.
+    </h1>
+
+    <form action="capturar_primero.php" method="post">
+        <label for="txtCantidad">Cantidad de Aves</label>
+        <input type="number" name="txtCantidad" id="txtCantidad" required>
+        <input type="submit" value="Enviar">
+    </form>
+
+    </center>
+</body>
+
+</html>
