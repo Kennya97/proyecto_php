d885e881ab28a1079d76f78fe167365bab387070
Kennya <castro21.kc@gmail.com>
2020-05-11 16:12:14 -0600

    Agregue capturar_primero.php

---
+++ capturar_primero.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang ="es">
+
+<head>
+<meta charset="UTF-8">
+<meta name ="viewport" content ="width=device-width, initial-scale=1.0">
+<title>Calculo Promedio</title>
+
+</head>
+<body bgcolor="#F5D0A9"><center>
+
+</center>
+</body>
+</html>
