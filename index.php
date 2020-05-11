2a34f766051a64827ecaa07635e9095ab86049f5
Kennya <castro21.kc@gmail.com>
2020-05-11 15:36:57 -0600

    Agregue un codigo en index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola</title>
+</head>
+<body bgcolor="#F3E2A9">
+<center>
+<h1>USO DE ARRAYS Y FUNSIONES</h1>
+<a href="index.php">Index</a> |
+<a href="primero.php">Primero</a> |
+<a href="segundo.php">Segundo</a> |
+
+<font>
+
+</center>
+    
+</body>
+</html>
